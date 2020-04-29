88776b9f0044116be1b8a240d9aebf6f004b5a75
Patrik <patrikmatos.tecinfo@gmail.com>
2020-04-29 10:51:09 -0300

    Adding hooks API comunication

--- src/view/Home/index.js
+++ src/view/Home/index.js
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
 import Box from "../../components/Box";
 import Header from "../../components/Header/index";
 import ShoppingTypes from "../../components/ShoppingTypes";
 import VehicleSearch from "../../components/VehicleSearch";
 
 const Home = () => {
+  const [fetchModel, setFetchModel] = useState([]);
+
+  useEffect(() => {
+    async function FetchModel() {
+      const response = await Axios.get(
+        "http://desafioonline.webmotors.com.br/api/OnlineChallenge/Model?MakeID=1"
+      );
+      console.log(response.data);
+    }
+    FetchModel();
+  }, []);
+
   return (
     <div className="container">
       <Box>
